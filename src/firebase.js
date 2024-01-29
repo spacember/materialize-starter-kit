42cd8e00fe75a6ed2b1f72f8229958634f8a7e58
aditya <aramdonee16@gmail.com>
2024-01-29 20:31:48 +0400

    Add firebase storage functionality

--- src/firebase.js
+++ src/firebase.js
@@ -1,5 +1,6 @@
 import { initializeApp } from 'firebase/app'
 import { getAuth } from 'firebase/auth'
+import { getStorage } from "firebase/storage";
 
 const firebaseConfig = {
   apiKey: 'AIzaSyA8PJQJ9FDJnk5-SQs03Tu5X5toVlh5hxU',
@@ -12,5 +13,6 @@ const firebaseConfig = {
 
 const app = initializeApp(firebaseConfig)
 const auth = getAuth(app)
+const storage = getStorage(app);
 
-export { app, auth }
+export { app, auth, storage }
