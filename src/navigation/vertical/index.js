5971022e45f96576b23b6b7c915a777685382eb9
aditya <aramdonee16@gmail.com>
2024-02-02 17:00:56 +0400

    Remove unused navigation items

--- src/navigation/vertical/index.js
+++ src/navigation/vertical/index.js
@@ -3,19 +3,7 @@ const navigation = () => {
     {
       title: 'Home',
       path: '/home',
-      icon: 'mdi:home-outline',
-    },
-    {
-      title: 'Second Page',
-      path: '/second-page',
-      icon: 'mdi:email-outline',
-    },
-    {
-      path: '/acl',
-      action: 'read',
-      subject: 'acl-page',
-      title: 'Access Control',
-      icon: 'mdi:shield-outline',
+      icon: 'mdi:home-outline'
     }
   ]
 }
