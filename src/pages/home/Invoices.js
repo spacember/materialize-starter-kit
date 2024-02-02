afdbb8d81b4c61e7803c7bf1b3c70402acc97a23
aditya <aramdonee16@gmail.com>
2024-02-02 17:01:47 +0400

    Refactor Invoices component to use MUI DataGrid and fetch user files from Firebase storage

--- src/pages/home/Invoices.js
+++ src/pages/home/Invoices.js
@@ -1,41 +1,59 @@
+// ** React Imports
+import React, { useEffect, useState } from 'react'
+
 // ** MUI imports
+import Box from '@mui/material/Box'
 import Card from '@mui/material/Card'
 import CardHeader from '@mui/material/CardHeader'
-import CardContent from '@mui/material/CardContent'
-import Table from '@mui/material/Table'
-import TableContainer from '@mui/material/TableContainer'
-import TableHead from '@mui/material/TableHead'
-import TableRow from '@mui/material/TableRow'
-import TableCell from '@mui/material/TableCell'
-import TableBody from '@mui/material/TableBody'
-import TablePagination from '@mui/material/TablePagination'
+import { DataGrid } from '@mui/x-data-grid'
 
 // ** Custom Components
 import Dialogue from './ui/Dialogue'
 
-const Invoices = ({ data, handlePageChange }) => (
-  <Card>
-    <CardHeader title='Documents' action={<Dialogue />}></CardHeader>
-    <CardContent>
-      <TableContainer>
-        <Table>
-          <TableHead>
-            <TableRow>
-              <TableCell>DOCUMENTS</TableCell>
-            </TableRow>
-          </TableHead>
-          <TableBody xs>
-            {data.map((item, index) => (
-              <TableRow key={index}>
-                <TableCell>{item}</TableCell>
-              </TableRow>
-            ))}
-          </TableBody>
-        </Table>
-      </TableContainer>
-      <TablePagination onPageChange={handlePageChange} />
-    </CardContent>
-  </Card>
-)
+// ** Firebase imports
+import { storage } from 'src/firebase'
+import { ref, listAll } from 'firebase/storage'
+
+// ** Context
+import { useContext } from 'react'
+import { AuthContext } from 'src/context/AuthContext'
+
+const columns = [
+  {
+    flex: 0.1,
+    field: 'file',
+    minWidth: 80,
+    headerName: 'Fichier'
+  }
+]
+
+const Invoices = () => {
+  // States
+  const { user } = useContext(AuthContext)
+  const [files, setFiles] = useState([])
+
+  // Get user files from storage
+  useEffect(() => {
+    const fetchStorageData = async () => {
+      const storageRef = ref(storage)
+      const folderRef = ref(storageRef, user.email)
+      const files = await listAll(folderRef)
+      const fileNames = files.items.map((file, index) => ({ file: file.name, id: index }))
+
+      setFiles(fileNames)
+    }
+
+    fetchStorageData()
+  }, [user.email])
+
+  return (
+    <Card>
+      <CardHeader title='Documents' action={<Dialogue />}></CardHeader>
+      <Box sx={{ height: 300, width: '100%' }}>
+        <DataGrid columns={columns} rows={files.slice(0, 10)} />
+      </Box>
+    </Card>
+  )
+}
 
 export default Invoices
