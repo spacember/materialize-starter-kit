147e97c87119a7ff7137183531cf0564291e20d2
aditya <aramdonee16@gmail.com>
2024-02-02 17:01:17 +0400

    Refactor home page component

--- src/pages/home/index.js
+++ src/pages/home/index.js
@@ -1,40 +1,15 @@
-import React, { useEffect, useState } from 'react'
-
-// ** CustomMUI imports
+// ** MUI imports
 import Grid from '@mui/material/Grid'
+
+// ** Custom Components
 import Invoices from './Invoices'
 import StatisticsCard from './StatisticsCard'
 
-// ** Firebase imports
-import { storage } from 'src/firebase'
-import { ref, listAll, getDownloadURL } from 'firebase/storage'
-
 const Home = () => {
-  const [data, setData] = useState([])
-
-  const handlePageChange = (event, newPage) => {
-    if (event && typeof newPage === 'number') {
-      // Handle page change logic here
-    }
-  }
-
-  useEffect(() => {
-    const fetchStorageData = async () => {
-      const storageRef = ref(storage)
-      const items = await listAll(storageRef)
-      const data = await Promise.all(items.items.map(item => getDownloadURL(item)))
-
-      console.log(data)
-      setData(data)
-    }
-
-    fetchStorageData()
-  }, [])
-
   return (
     <Grid container spacing={6}>
       <Grid item xs={12}>
-        <Invoices data={data} handlePageChange={handlePageChange} />
+        <Invoices />
       </Grid>
       <Grid item xs={6}>
         <StatisticsCard />
